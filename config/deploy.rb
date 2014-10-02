a5bba03819fe15252cae53967b3e316cb599aacd
suras <msurendar@outlook.com>
2014-10-02 19:17:59 +0530

    capfile changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, 'git@bitbucket.org:yelo/yeloapp.git'
  set :deploy_to, '/home/deploy/yelo'
 
  set :pty,  false
+ 
+ set :rvm1_ruby_version, "2.1.2"
 
  # set :sidekiq_config, "#{current_path}/config/sidekiq.yml"
  # set :sidekiq_env, "production"
@@ -31,18 +33,8 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
-      #execute "cd '#{current_path}' && bundle exec thin restart -C /etc/thin/yelorailsapp.yml"
     end
-   # on roles(:app), :except => { :no_release => true } do
-   #    within release_path do
-   #      with rails_env: fetch(:rails_env) do
-   #        execute "bundle exec thin restart -C /etc/thin/yelorailsapp.yml"
-   #      end
-   #    end
-   #  end
   end
-
-
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
