e4829ef0500c8eccd3f2603fe56e13dd2197d6ef
suras <msurendar@outlook.com>
2014-10-21 13:25:13 +0530

    seed previous data from lovocal

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+JSON.parse(open("#{Rails.root}/db/sub_categories.json").read).each do |s|
+  t = Tag.where(name: s["name"], _id: s["_id"]["$oid"]).first_or_create 
+end
+
+
+JSON.parse(open("#{Rails.root}/db/users.json").read).each do |s|
+  num = User.mobile_number_format(s["mobile_number"])
+  mobile = num[:mobile_number]
+  country_code = num[:country_code]
+  user = User.where(mobile_number: mobile).first
+  if(user.blank?)
+  	user = User.new
+  	user.id = s["_id"]["$oid"]
+  	user.mobile_number = mobile
+    user.name = s["name"]
+    user.description = s["description"]
+    user.country_code = country_code
+    user.save
+  end
+end
+
+
+JSON.parse(open("#{Rails.root}/db/listings.json").read).each do |s|
+  list = Listing.where(user_id: s["_id"]["$oid"]).first
+  if(list.blank?)
+	  list = Listing.new
+	  list.user_id = s["_id"]["$oid"]
+	  list.latitude = s["latitude"]
+	  list.longitude = s["longitude"]
+	  list.country = s["country"]
+	  list.city = s["city"]
+	  list.country = s["counrty"]
+	  list.save
+	end
+	list.create_tags(s["sub_cat_ids"])
+end
+
+
+
