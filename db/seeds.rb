cf852100ed2257fa13ddd79577d22da1b86ccbce
suras <msurendar@outlook.com>
2014-10-27 22:37:55 +0530

    tag seed change

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,10 @@
 
 JSON.parse(open("#{Rails.root}/db/tags.json").read).each do |s|
    group = Group.where(name: s.keys.first).first_or_create
-   s[s.keys.first].each do |t|
-      puts "#{s.keys.first}#{t}"
+   # s[s.keys.first].each do |t|
+      # puts "#{s.keys.first}#{t}"
       group.tags.where(name: t).first_or_create
-   end
+   # end
 end
 
   admin_user = User.where(email: Rails.application.secrets.admin_email).first
