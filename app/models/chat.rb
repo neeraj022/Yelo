c844f82e6efe225b42a2fda7b01c2a889dd7a688
suras <msurendar@outlook.com>
2014-11-02 18:08:13 +0530

    chat wall count error fix

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -16,7 +16,7 @@ class Chat
       chat = Chat.create(receiver_id: params[:sender_id], receiver_id: params[:receiver_id],
        message: params[:message], reply_id: params[:reply_id], is_seen: false)
       if(params[:wall_id].present?)
-         Wall.save_wall_chat_user(params[:wall_id], params[:receiver_id])
+         Wall.save_wall_chat_user(params[:wall_id], params[:sender_id])
       end
       chat
     end
