bc1e6f77e66e253c86fbd8c782aca17a00f72e94
suras <msurendar@outlook.com>
2014-11-09 23:54:44 +0530

    enable sidekiq and cntact notification

--- app/workers/contact_wall_worker.rb
+++ app/workers/contact_wall_worker.rb
@@ -1,6 +1,6 @@
 class ContactWallWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "contactwall", retry: false
+  sidekiq_options queue: "contact_wall", retry: false
 
   def perform(user_id, wall_id)
     Notification.save_contact_wall(user_id, wall_id)
