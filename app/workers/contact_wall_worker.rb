5324a19a794ff20198500886fe80fbb0bbfaa330
suras <msurendar@outlook.com>
2014-11-10 15:46:42 +0530

    contact wall worker method argument fix

--- app/workers/contact_wall_worker.rb
+++ app/workers/contact_wall_worker.rb
@@ -2,8 +2,8 @@ class ContactWallWorker
   include Sidekiq::Worker
   sidekiq_options queue: "contact_wall", retry: false
 
-  def perform(user_id, wall_id)
-    Notification.save_contact_wall(user_id, wall_id)
+  def perform(wall_id)
+    Notification.save_contact_wall(wall_id)
   end
 
 end
