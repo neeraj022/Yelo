54cc22b12d42afcb1483ffd406b839c9c8cc1004
suras <msurendar@outlook.com>
2014-11-24 17:07:35 +0530

    serial code fix in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -180,7 +180,7 @@ class Api::V1::UsersController < Api::V1::BaseController
       # @sms = @user.send_sms
       # if(@sms[:status])
         encrypt_key = Rails.application.secrets.encrypt_key
-        key = AESCrypt.encrypt(@user.sms_serial_key, encrypt_key)
+        key = AESCrypt.encrypt(@user.serial_code, encrypt_key)
         render json: {status: Code[:status_success], serial_code: key}
       # else
       #   render json: {status: Code[:status_error], serial_code: @user.serial_code, error_message: @sms[:error_message]}
