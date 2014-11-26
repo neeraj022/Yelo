2eac8ee4152d50e07934d2992aab983462612eb0
suras <msurendar@outlook.com>
2014-11-26 23:54:45 +0530

    enable encryption

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -181,7 +181,7 @@ class Api::V1::UsersController < Api::V1::BaseController
       # if(@sms[:status])
         key = Code.encrypt_aes(@user.serial_code.to_s)
         # serial_code: key[2]
-        render json: {status: Code[:status_success], serial_code: @user.serial_code.to_s}
+        render json: {status: Code[:status_success], serial_code: key[2]}
       # else
       #   render json: {status: Code[:status_error], serial_code: @user.serial_code, error_message: @sms[:error_message]}
       # end
