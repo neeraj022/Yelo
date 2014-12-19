d3b861d8dc4942fca885a4782c75ce334a526d84
suras <msurendar@outlook.com>
2014-12-19 16:37:37 +0530

    email message change

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,10 +3,12 @@ class Mailer < ActionMailer::Base
   default from: 'startupnow@yelo.red'
   default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@yelo.red"
 
-  def refer(email, msg, referred_by)
+  def refer(email, msg, by, to, tag)
     return false unless email =~ Code.email_regex
   	 @msg = msg
-  	 @referred_by = referred_by
-     mail(to: email, subject: "#{@referred_by} referred you on yelo")
+  	 @referred_by = by
+     @referrer = to
+     @tag = tag
+     mail(to: email, subject: "#{@referred_by} has recommended you for #{@tag}")
   end
 end
--- app/workers/email_worker.rb
+++ app/workers/email_worker.rb
@@ -2,10 +2,10 @@ class EmailWorker
   include Sidekiq::Worker
   sidekiq_options queue: "email", retry: false
 
-  def perform(type, email, msg, by=nil)
+  def perform(type, email, msg, by=nil, to=nil, tag=nil)
   	case type
   	when "refer"
-      Mailer.refer(email, msg, by).deliver
+      Mailer.refer(email, msg, by, to, tag).deliver
     end
   end
 
