d013ac7ac6e243e4565071925c070474cc295bab
suras <msurendar@outlook.com>
2015-02-06 16:03:55 +0530

    oid issue is custom serializer

--- app/serializers/user_profile_serializer.rb
+++ app/serializers/user_profile_serializer.rb
@@ -1,4 +1,4 @@
-class UserProfileSerializer < ActiveModel::Serializer
+class UserProfileSerializer < CustomSerializer
   attributes :id, :name, :description, :image_url, :rating_avg, :total_tagged, 
              :total_ratings, :connects_count, :share_token, :platform_version, :profile_image
 end
