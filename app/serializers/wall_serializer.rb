7a93549846f696c58a29454405736eb6ad1a259e
suras <msurendar@outlook.com>
2015-02-09 18:52:33 +0530

    chat users in wall res

--- app/serializers/wall_serializer.rb
+++ app/serializers/wall_serializer.rb
@@ -1,8 +1,12 @@
 class WallSerializer < CustomSerializer
   attributes :id, :message, :chat_users_count, :tagged_users_count, :created_at,
              :tag_name, :country, :city, :state, :address, :status, :tmp_id, :tag_id, :group_id,
-             :keywords, :group_name
+             :keywords, :group_name, :wall_chats
   has_one  :wall_image
   has_one  :wall_owner
   has_many :wall_items
+
+  def wall_chats
+    chat_users = User.get_users(object.chat_user_ids)
+  end
 end
