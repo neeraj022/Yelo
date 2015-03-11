f2063c82142d8e84ac11ce9400b82e1e560ca753
suras <msurendar@outlook.com>
2015-03-11 18:48:05 +0530

    claim corerections

--- app/workers/claim_worker.rb
+++ app/workers/claim_worker.rb
@@ -3,9 +3,7 @@ class ClaimWorker
   sidekiq_options retry: false
 
   def perform(user_id, mobile_num)
-  	user = User.where(_id: user_id).first
-  	num = Rails.application.secrets.w_mobile_number
-  	puts "is present"+num
+  	user = User.where(_id: user_id).first 
     yelo = User.where(mobile_number: mobile_num).first
     msg = "Your claim has been processed. you will hear from us in a week"
     User.send_chat_message(yelo, user, msg)
