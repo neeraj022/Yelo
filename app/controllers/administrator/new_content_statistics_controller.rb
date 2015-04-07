880dc17b09e51f3405ca4d47efa75be1dbf4d6f0
suras <msurendar@outlook.com>
2015-04-07 21:12:58 +0530

    previous day content count

--- app/controllers/administrator/new_content_statistics_controller.rb
+++ app/controllers/administrator/new_content_statistics_controller.rb
@@ -1,28 +1,28 @@
 class Administrator::NewContentStatisticsController  < Administrator::AdministratorController
   
   def index
-  	 @date = Date.today
-  	 @day_query = "{created_at: @date.beginning_of_day..@date.end_of_day}"
-  	 @week_query = "{created_at: @date.beginning_of_week..@date.end_of_day}"
-  	 @month_query = "{created_at: @date.beginning_of_month..@date.end_of_month}"
-     ######################### day ###########################################
-     @day_listings = Listing.where(eval(@day_query))
-     @day_users =  User.where(eval(@day_query)).where(mobile_verified: true)
-     @day_chats = Chat.where(eval(@day_query))
-     @day_posts = Wall.where(eval(@day_query))
-     @day_service_cards = ServiceCard.where(eval(@day_query))
-     ##################### week  #######################################
-     @week_listings = Listing.where(eval(@week_query))
-     @week_users =  User.where(eval(@week_query)).where(mobile_verified: true)
-     @week_chats = Chat.where(eval(@week_query))
-     @week_posts = Wall.where(eval(@week_query))
-     @week_service_cards =  ServiceCard.where(eval(@week_query))
-     ################## month  #######################################
-     @month_listings = Listing.where(eval(@month_query))
-     @month_users =  User.where(eval(@month_query)).where(mobile_verified: true)
-     @month_chats = Chat.where(eval(@month_query))
-     @month_posts = Wall.where(eval(@month_query))
-     @month_service_cards =  ServiceCard.where(eval(@month_query))
+    @date = Date.parse(1.day.ago.to_s)
+    @day_query = "{created_at: @date.beginning_of_day..@date.end_of_day}"
+    @week_query = "{created_at: @date.beginning_of_week..@date.end_of_day}"
+    @month_query = "{created_at: @date.beginning_of_month..@date.end_of_month}"
+    ######################### day ###########################################
+    @day_listings = Listing.where(eval(@day_query))
+    @day_users =  User.where(eval(@day_query)).where(mobile_verified: true)
+    @day_chats = Chat.where(eval(@day_query))
+    @day_posts = Wall.where(eval(@day_query))
+    @day_service_cards = ServiceCard.where(eval(@day_query))
+    ##################### week  #######################################
+    @week_listings = Listing.where(eval(@week_query))
+    @week_users =  User.where(eval(@week_query)).where(mobile_verified: true)
+    @week_chats = Chat.where(eval(@week_query))
+    @week_posts = Wall.where(eval(@week_query))
+    @week_service_cards =  ServiceCard.where(eval(@week_query))
+    ################## month  #######################################
+    @month_listings = Listing.where(eval(@month_query))
+    @month_users =  User.where(eval(@month_query)).where(mobile_verified: true)
+    @month_chats = Chat.where(eval(@month_query))
+    @month_posts = Wall.where(eval(@month_query))
+    @month_service_cards =  ServiceCard.where(eval(@month_query))
   end
 
   def set_date
@@ -35,7 +35,7 @@ class Administrator::NewContentStatisticsController  < Administrator::Administra
 			    when "month"
 			      {from: date.beginning_of_month, to: date.end_of_month}
 			    else
-                  {from: date.beginning_of_day, to: date.end_of_day}
+            {from: date.beginning_of_day, to: date.end_of_day}
 			    end
     date_hash
   end
